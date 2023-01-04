f8582451f28c454e4972e8b4f16fe5ccd293bf4c
Arka102202 <111365238+Arka102202@users.noreply.github.com>
2023-01-04 21:27:59 +0530

    initial commit

---
+++ spring-ticket-management-demo-1/src/main/java/com/arkeeta/ticketManagement/SpringTicketManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.arkeeta.ticketManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTicketManagementDemoApplication.class, args);
+	}
+
+}
